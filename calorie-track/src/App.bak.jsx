6fcd51cbf78932cbbef6e9e08f3f9a02c475c479
mingww64 <i@feli.cf>
2025-11-16 07:27:46 +0000

    init backend

---
+++ calorie-track/src/App.bak.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import HomeScreen from './components/HomeScreen.jsx'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HomeScreen/>
+      </div>
+    </>
+  )
+}
+
+export default App
